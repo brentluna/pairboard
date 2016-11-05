979f25d7a1dbeb390f9d00e1569327f2c3a85592
brentluna <brentadamluna@gmail.com>
2016-11-04 22:06:51 -0700

    naive longest common substring

--- d1.js
+++ d1.js
@@ -41,3 +41,27 @@ function shiftChar(c, shift) {
 		return String.fromCharCode(((charCodeAt - 97 + shift) % 26 + 97));
 	}
 }
+
+/*
+Write a function, longest_common_substring(str1, str2) that
+ takes two strings and returns the longest common substring.
+  A substring is defined as any consecutive slice of letters
+   from another string.
+
+Bonus: solve it in O(m * n) using O(m * n) extra space.
+ (Hint: the solution involves dynamic programming which
+  will be introduced later in the course.)
+*/
+
+function longestCommonSubStr(str1, str2) {
+	let longest = '';
+	for (let i = 0; i < str1.length; i++) {
+		for (let j = i; j < str1.length; j++) {
+			let str = str1.slice(i, j);
+			if (str.length > longest.length && str2.indexOf(str) !== -1) {
+				longest = str;
+			}
+		}
+	}
+	return longest;
+}
