b41f1a04d551238190a4dad2e7e155e2fc13f765
Brent Luna <brentadamluna@gmail.com>
2016-11-06 19:34:50 -0800

    added folding cipher function

--- d3.js
+++ d3.js
@@ -8,3 +8,32 @@ Array.prototype.myMap =  function(cb) {
 	});
 	return result;
 }
+
+
+/*Implement the Folding Cipher. It folds the alphabet in half
+ and uses the adjacent letter. Ie. a <=> z, b <=> y, c <=> x, m <=> n.
+*/
+
+function foldingCipher(str) {
+	let result = '';
+	for (let i = 0; i < str.length; i++) {
+		let char = str[i];
+		if (char === ' ') {
+			result += ' ';
+		} else {
+			result += swapChar(char);
+		}
+	}
+	return result;
+}
+
+function swapChar(char) {
+	let val = char.charCodeAt();
+	let diff = 97 - val;
+	if (diff < 13) {
+	 	return String.fromCharCode(diff + (26 - diff) + 96);
+	} else {
+		return String.fromCharCode(26 - diff + 96);
+	}
+}
+
