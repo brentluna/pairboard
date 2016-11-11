5e103776482b9b36331eca702f3b55a4b97ca7bd
Brent Luna <brentadamluna@gmail.com>
2016-11-10 22:09:58 -0800

    added move zeros and look and say functions

--- d7.js
+++ d7.js
@@ -74,3 +74,62 @@ function weightedRandomIndex(arr) {
 		if (rand < cummulativeSum) return i;
 	}
 }
+
+/*
+
+Given an array, move all zeros to the end. The order of non-zero elements 
+does not matter. Ex:
+
+move_zeros([1, 2, 0, 3, 4, 0, 5, 6, 0]) == [1, 2, 6, 3, 4, 5, 0, 0, 0]
+
+Algorithm should be O(n); use O(1) extra space.
+
+*/
+
+function moveZeros(arr) {
+	let lastZeroIdx = arr.length - 1;
+	for(let i = arr.length - 1; i >= 0; i--) {
+		let currEl = arr[i];
+		if (currEl === 0) {
+			let tmp = arr[lastZeroIdx];
+			arr[lastZeroIdx] = 0;
+			arr[i] = tmp;
+			lastZeroIdx--;
+		}
+	}
+	return arr;
+}
+
+/* 
+Implement the 'look and say' function. 'Look and say' takes an input array
+ and outputs an array that describes the count of the elements in the input 
+ array as they appear in order.
+
+
+Example:
+
+# there is one '1' in the input array
+look_and_say([1]) == [[1, 1]]
+
+# there are two '1's in the input array
+look_and_say([1, 1]) == [[2, 1]]
+
+# there is one '2', followed by one '1' in the input array
+look_and_say([2, 1]) == [[1, 2], [1, 1]]
+
+# is one '1', followed by one '2', followed by 2 '1's in the input
+# array
+look_and_say([1, 2, 1, 1]) == [[1, 1], [1, 2], [2, 1]]
+*/
+
+function lookAndSay(arr) {
+	let counter = [[1, arr[0]]];
+	for (let i = 1; i < arr.length; i++) {
+		if (arr[i] === counter[counter.length - 1][1]) {
+			counter[counter.length -1][0]++
+		} else {
+			counter.push([1, arr[i]]);
+		}
+	}
+	return counter;
+}
