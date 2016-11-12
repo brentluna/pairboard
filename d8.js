af6ad1bc3c40e431c2687e3656f306249c51171d
Brent Luna <brentadamluna@gmail.com>
2016-11-11 20:24:02 -0800

    added k closest stars, with the assumption of a pre built binary max heap

--- d8.js
+++ d8.js
@@ -38,3 +38,46 @@ function sumUponSums3(arr) {
 	}
 	return Math.abs(sum);
 }
+
+/*
+Consider a coordinate system for the Milky Way, in which Earth is at (0,0,0).
+ Model stars as points, and assume distances are in light years. The Milky
+  Way consists of approximately 10^12 stars, and their coordinates are stored 
+  in a file. How would you compute the k stars which are closest to Earth?
+
+Hint: Suppose you know the k closest stars in the first n stars. If the 
+(n + 1)th star is to be added to the set of k closest stars, which element
+ in that set should be evicted?
+*/
+
+function kClosestStars(stars, k) {
+	//assuming we had a max Heap, you could push k stars into the max heap
+	// once you have the mxnheap filled w/ k stars, you could continue to push 
+	// 1 star at a time, and extract 1 from the max heap, until the stars array is empty
+	// once it's empty, you would have a maxHeap of k elements, which since exxtracting 
+	// each time always took the greatest value, you would be left w/ the k smallest distances
+
+
+	// I don't have a max heap, so this code wont' work
+	let distanceFromEarth = (a, b) => {
+  	let dist1 = Math.sqrt(Math.pow(a.x, 2) + Math.pow(a.y, 2) + Math.pow(a.z, 2))
+  	let dist2 = Math.sqrt(Math.pow(b.x, 2) + Math.pow(b.y, 2) + Math.pow(b.z, 2))
+  	return Math.sign(dist1 - dist2);
+	});
+
+	let maxHeap = new MaxHeap(distanceFromEarth);
+  for (let i = 0; i < k; i++) {
+  	let currStar = stars.push();
+  	maxHeap.insert(currStar);
+  }
+  while (stars.length) {
+  	let currStar = = stars.push();
+  	maxHeap.insert(currStar);
+  	maxHeap.extract();
+  }
+  let kClosest = [];
+  for (let i = 0; i < k; i++) {
+  	kClosest.push(maxHeap.extract());
+  };
+  return kClosest;
+}
