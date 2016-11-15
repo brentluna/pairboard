0b4edea30f11e229d3c0b914f3892494710e26df
Brent Luna <brentadamluna@gmail.com>
2016-11-15 14:01:28 -0800

    added an iterative, recursive, and tail recursive factorial functions

--- d10.js
+++ d10.js
@@ -13,3 +13,29 @@ function toBinary(num) {
 	}
 	return binary.length ? binary.join('') : '0';
 }
+
+/*
+Implement factorial with and without recursion. 
+What is a potential disadvantage of the recursive way?
+
+What is tail-recursion? Does Ruby have tail-call optimization?
+ Pretend it did; write a tail-recursive version of rec_fac.
+*/
+
+function factRec(num) {
+	if (num === 1) return 1;
+	return num * (factRec(num - 1));
+}
+
+function factIter(num) {
+	let result = 1;
+	for (let i = 2; i < num; i++) {
+		result *= num;
+	}
+	return result;
+}
+
+function factTailRec(num, product = 1) {
+	if (num === 1) return product;
+	return factTailRec(num - 1, product * num);
+}
