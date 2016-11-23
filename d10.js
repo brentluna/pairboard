3d8f84ebf7f7e986337795cfbfb7d03f26c32261
Brent Luna <brentadamluna@gmail.com>
2016-11-22 17:58:14 -0800

    finished greatest p sub string

--- d10.js
+++ d10.js
@@ -39,3 +39,83 @@ function factTailRec(num, product = 1) {
 	if (num === 1) return product;
 	return factTailRec(num - 1, product * num);
 }
+
+
+/*
+Given a string, find the lexicographically greatest pseudo-substring.
+
+Example (read on for further explanation):
+
+max_unique_psub('abcdef')
+=> 'f'
+
+max_unique_psub('abcdefedcba')
+=> 'fedcba'
+
+max_unique_psub('algorithms')
+=> 'ts'
+Let's define a pseudo-substring: psub is any substring that is ordered by index.
+
+For example:
+
+"ac" is a psub of "acb"
+"cb" is a psub of "acb"
+"bc" is _not_ a psub of "acb" (letters are out of order)
+
+psubs("acb") == [
+  "a",
+  "ac",
+  "acb",
+  "c",
+  "cb",
+  "b"
+]
+Next, let's define lexicographical order:
+
+str1 > str2 IF
+(a) str1 != str2 AND EITHER
+(b1) str2 is a prefix of str1 OR
+(b2) at the first position at which str1 and str2 differ (say i), str1[i] > str2[i].
+For instance: "abc" > "ab" and "acb" > "abc".
+
+With this information, given a string str, find the lexicographical greatest psubstring.
+
+Solve it first by generating all psubstrings and picking the greatest (in Big-Oh, how many are there?).
+
+Next, improve your algorithm to do this in O(n) time.
+*/
+
+function subSets(strArr) {
+  if (strArr.length === 0) return [[]];
+  let firstEl = strArr[0];
+  let rest = strArr.slice(1);
+  let prev = subSets(rest);
+  let newSubs = prev.map(el => el.concat([firstEl]));
+  newSubs = prev.concat(newSubs);
+  return newSubs;
+}
+function pSub1(str) {
+  let subs = subSets(str.split(''));
+  subs = subs.map(sub => sub.join(''));
+  let greatest = '';
+  subs.forEach(sub => {
+    if (sub > greatest){
+      greatest = sub;
+    }
+  });
+  return greatest;
+}
+
+
+function greatestPSub(str){
+  let greatest = str[str.length - 1];
+  for (let i = str.length - 2; i > 0; i--) {
+    if (str[i] > greatest[0]) {
+      greatest = str[i] + greatest;
+    }
+  }
+  return greatest;
+}
+
+
+hello
